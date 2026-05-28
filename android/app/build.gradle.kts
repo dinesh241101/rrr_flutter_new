e7ba5e67ccdfa961e9ef38e20189662c3d533ded
Dinesh <dinesh2411kaushik@gmail.com>
2026-05-28 10:17:03 +0530

    build errror fix

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -30,6 +30,15 @@ android {
         versionName = flutter.versionName
     }
 
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+    }
+
+    kotlinOptions {
+        jvmTarget = "17"
+    }
+
     buildTypes {
         release {
             // TODO: Add your own signing config for the release build.
